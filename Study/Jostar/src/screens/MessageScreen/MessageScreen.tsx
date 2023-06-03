f23005d0d53820a8296090a070adc8acaa8584a1
HoCG <rhwkd5012@naver.com>
2023-06-03 23:47:53 +0900

    feat #1 구현상에 필요한 각 화면들 추가

---
+++ Study/Jostar/src/screens/MessageScreen/MessageScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const MessageScreen = () => {
+  return (
+    <View>
+      
+    </View>
+  );
+};
+
+export default MessageScreen;
---
+++ Study/Jostar/src/screens/NotificationScreen/NotificationScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const AlertScreen = () => {
+  return (
+    <View>
+      
+    </View>
+  );
+};
+
+export default AlertScreen;
