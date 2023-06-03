4f2c96626d6482f449efa946fe1e702fd401ec38
HoCG <rhwkd5012@naver.com>
2023-06-03 23:53:57 +0900

    feat #1 스택 네이게이션을 통해 디테일 화면 네이게이팅 구현

---
+++ Study/Jostar/src/screens/HomeScreen/HomeScreen.style.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+import { mixinStyles } from "styles/mixinStyle";
+
+export const homeScreenStyles = StyleSheet.create({
+  container: {
+    ...mixinStyles.container
+  }
+});
