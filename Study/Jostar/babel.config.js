870a66e7b71bea4eaaccb944410747f30344ae2d
HoCG <rhwkd5012@naver.com>
2023-06-03 22:23:06 +0900

    feat #0 절대경로 지정을 위한 module-resolver 설정

--- Study/Jostar/babel.config.js
+++ Study/Jostar/babel.config.js
@@ -2,5 +2,24 @@ module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
   plugins: [
     ["babel-plugin-react-docgen-typescript", { exclude: "node_modules" }],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.style.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.test.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+      },
+    ],
   ],
 };
