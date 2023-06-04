46b66095d314fbcbfaaf7a14495ee46884e71cb7
HoCG <rhwkd5012@naver.com>
2023-06-04 14:29:24 +0900

    feat #1 (과제3번) Zustand가 적용된 카운터 카드 구현

---
+++ Study/Jostar/src/components/molecules/cards/CounterCard/CounterCard.test.tsx
@@ -0,0 +1,35 @@
+import {
+  render,
+  fireEvent,
+  waitFor,
+  renderHook,
+  act,
+} from "@testing-library/react-native";
+import CounterCard from "./CounterCard";
+import { useCountStore } from "store/count";
+
+// const initRender = () => {
+//   const { count } = useCountStore();
+//   return count;
+// };
+
+describe("컴포넌트/원자/카드/카운터카드", () => {
+  test("+ 버튼을 누를 경우, 카운트값이 1인지 테스트", async () => {
+    const current = renderHook(() => useCountStore());
+    const screen = render(<CounterCard />);
+    const increaseButton = screen.getByTestId("colorButton-증가");
+    fireEvent.press(increaseButton);
+    await waitFor(() => expect(current.result.current.count).toBe(1));
+  });
+  test("+ + + - 버튼을 누를 경우, 카운트값이 1인지 테스트", async () => {
+    const current = renderHook(() => useCountStore());
+    const screen = render(<CounterCard />);
+    const increaseButton = screen.getByTestId("colorButton-증가");
+    const decreaseButton = screen.getByTestId("colorButton-감소");
+    fireEvent.press(increaseButton);
+    fireEvent.press(increaseButton);
+    fireEvent.press(increaseButton);
+    fireEvent.press(decreaseButton);
+    await waitFor(() => expect(current.result.current.count).toBe(2));
+  });
+});
