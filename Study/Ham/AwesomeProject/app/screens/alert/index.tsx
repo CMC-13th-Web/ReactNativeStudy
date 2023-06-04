a90a74b40e029fc5d634fed000b02137cee4e04c
hamo-o <yheel369@gmail.com>
2023-06-04 18:21:40 +0900

    Feat: add screens

---
+++ Study/Ham/AwesomeProject/app/screens/alert/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+const Alert = () => {
+  return (
+    <View>
+      <Text>AlertScreen</Text>
+    </View>
+  );
+};
+
+export default Alert;
