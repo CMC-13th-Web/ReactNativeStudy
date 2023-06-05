743aa230606830ab0fb2a8f4339ec2ed50dd479c
Yeonju Jo <yjj0287@gmail.com>
2023-06-05 21:33:15 +0900

    feat #2 SafeAreaView 추가

--- Study/Jane/Project/week01/App.tsx
+++ Study/Jane/Project/week01/App.tsx
@@ -8,13 +8,20 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import RootStack from 'navigation/RootStack';
+import {styled} from 'styled-components/native';
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <RootStack />
+      <StyledSafeAreaView>
+        <RootStack />
+      </StyledSafeAreaView>
     </NavigationContainer>
   );
 }
 
 export default App;
+
+const StyledSafeAreaView = styled.SafeAreaView`
+  flex: 1;
+`;
