4b4a077f97209f48cec2563f7490cfe8f1a050b2
kongnayeon <seoultownroad@naver.com>
2023-06-06 02:04:52 +0900

    feat: 아이콘 적용

--- Study/JJang/src/components/common/TabNavigation/index.tsx
+++ Study/JJang/src/components/common/TabNavigation/index.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable react/no-unstable-nested-components */
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
 import {AlertScreen} from '../../../screens/alert';
 import {MessageScreen} from '../../../screens/message';
 import {SearchScreen} from '../../../screens/search';
 import {StackNavigation} from '../StackNavigation/index';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,6 +19,9 @@ export const TabNavigation = () => {
         component={StackNavigation}
         options={{
           title: '홈',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" size={size} color={color} />
+          ),
         }}
       />
       <Tab.Screen
@@ -24,6 +29,9 @@ export const TabNavigation = () => {
         component={AlertScreen}
         options={{
           title: '알림',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="bell" size={size} color={color} />
+          ),
         }}
       />
       <Tab.Screen
@@ -31,6 +39,9 @@ export const TabNavigation = () => {
         component={SearchScreen}
         options={{
           title: '검색',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="search" size={size} color={color} />
+          ),
         }}
       />
       <Tab.Screen
@@ -38,6 +49,9 @@ export const TabNavigation = () => {
         component={MessageScreen}
         options={{
           title: '메시지',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="wechat" size={size} color={color} />
+          ),
         }}
       />
     </Tab.Navigator>
