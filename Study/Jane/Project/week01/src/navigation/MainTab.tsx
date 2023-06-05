cdbf121b4cb30f759a4a4901fa2ff5b78968c1d4
Yeonju Jo <yjj0287@gmail.com>
2023-06-05 15:55:51 +0900

    design #1 하단바 아이콘 추가

--- Study/Jane/Project/week01/src/navigation/MainTab.tsx
+++ Study/Jane/Project/week01/src/navigation/MainTab.tsx
@@ -7,6 +7,7 @@ import {
   NavigatorScreenParams,
 } from '@react-navigation/native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {RootStackNavigationProp} from './RootStack';
 import SearchScreen from 'screens/SearchScreen';
 import NotificationScreen from 'screens/NotificationScreen';
@@ -15,8 +16,8 @@ import HomeScreen from 'screens/HomeScreen';
 
 type MainTabParamList = {
   Home: undefined;
-  Search: undefined;
   Notification: undefined;
+  Search: undefined;
   Message: undefined;
 };
 
@@ -33,21 +34,46 @@ const Tab = createBottomTabNavigator<MainTabParamList>();
 export default function MainTab() {
   return (
     <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="Home" component={HomeScreen} options={{title: '홈'}} />
       <Tab.Screen
-        name="Search"
-        component={SearchScreen}
-        options={{title: '검색'}}
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: '홈',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Notification"
         component={NotificationScreen}
-        options={{title: '알림'}}
+        options={{
+          title: '알림',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="notifications" color={color} size={size} />
+          ),
+        }}
       />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          title: '검색',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="search" color={color} size={size} />
+          ),
+        }}
+      />
+
       <Tab.Screen
         name="Message"
         component={MessageScreen}
-        options={{title: '메시지'}}
+        options={{
+          title: '메시지',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="message" color={color} size={size} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
