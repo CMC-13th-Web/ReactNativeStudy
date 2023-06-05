f907012e932dd6fdaf207cf6ee475234a9381729
hamo-o <yheel369@gmail.com>
2023-06-05 13:29:19 +0900

    Feat: add counter

--- Study/Ham/AwesomeProject/app/screens/search/index.tsx
+++ Study/Ham/AwesomeProject/app/screens/search/index.tsx
@@ -1,12 +1,44 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import styled from 'styled-components/native';
+import {Button, Text} from 'react-native';
+import {useRecoilState} from 'recoil';
+
+import {counter} from '../../states/counter';
 
 const Search = () => {
+  const [count, setCount] = useRecoilState(counter);
+
   return (
-    <View>
+    <Container>
       <Text>SearchScreen</Text>
-    </View>
+      <Counter>
+        <TitleContainer>
+          <Title>{count}</Title>
+        </TitleContainer>
+        <ButtonContainer>
+          <Button title="+1" onPress={() => setCount(count + 1)} />
+          <Button title="-1" onPress={() => setCount(count - 1)} />
+        </ButtonContainer>
+      </Counter>
+    </Container>
   );
 };
+
+const Container = styled.View`
+  flex: 1;
+`;
+const Counter = styled.View`
+  flex: 1;
+  justify-content: space-between;
+  align-items: center;
+`;
+const TitleContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+const Title = styled.Text`
+  font-size: 100px;
+`;
+const ButtonContainer = styled.View``;
 
 export default Search;
