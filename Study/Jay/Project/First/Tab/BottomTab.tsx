b12a943553a24d49098e4a6446dd8fcced97d289
신재훈 <sjhh0105@naver.com>
2023-06-05 22:28:08 +0900

    feat: 하단탭

---
+++ Study/Jay/Project/First/Tab/BottomTab.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeStackScreen from '../Pages/Home/HomeStackScreen';
+import NotificationsScreen from '../Pages/Notification/NotificationsScreen';
+import SearchScreen from '../Pages/Search/SearchScreen';
+import MessagesScreen from '../Pages/Message/MessagesScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTab() {
+  return (
+    <Tab.Navigator
+      screenOptions={() => ({
+        tabBarActiveTintColor: 'rgb(47, 124, 246)',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen
+        name="홈"
+        component={HomeStackScreen}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen name="알림" component={NotificationsScreen} />
+      <Tab.Screen name="검색" component={SearchScreen} />
+      <Tab.Screen name="메세지" component={MessagesScreen} />
+    </Tab.Navigator>
+  );
+}
