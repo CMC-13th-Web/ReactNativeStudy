d6063c54bedff559cc92e36bd457a79fda246b4e
kongnayeon <seoultownroad@naver.com>
2023-06-05 20:30:11 +0900

    rename: Stack Navigator name 변경

--- Study/JJang/App.tsx
+++ Study/JJang/App.tsx
@@ -1,5 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
 import React from 'react';
 
 import {AlertScreen} from './src/screens/alert';
@@ -13,10 +15,10 @@ function App(): JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="home" component={HomeScreen} />
-        <Stack.Screen name="alert" component={AlertScreen} />
-        <Stack.Screen name="search" component={SearchScreen} />
-        <Stack.Screen name="message" component={MessageScreen} />
+        <Stack.Screen name="홈" component={HomeScreen} />
+        <Stack.Screen name="알림" component={AlertScreen} />
+        <Stack.Screen name="검색" component={SearchScreen} />
+        <Stack.Screen name="메시지" component={MessageScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
