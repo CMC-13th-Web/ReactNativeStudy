7d23eeb2c1e4d439c2c835524601c1cfd877e9a0
신재훈 <sjhh0105@naver.com>
2023-06-05 23:27:13 +0900

    feat: box 컴포넌트 제어 (과제 1-2)

---
+++ Study/Jay/Project/First/Pages/Detail/DetailScreen.styles.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 10,
+    paddingLeft: 10,
+    width: 200,
+  },
+});
