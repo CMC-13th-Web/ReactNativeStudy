2784d62537e62f8a34326f72803dd8e5ff65a92a
Hanna922 <hanna.urssu@gmail.com>
2023-06-11 22:22:14 +0900

    feat #1 카메라, 사진 선택 후 불러오기 기능 구현
    - 앱 초기 진입 시 RequestPermission()
    - Upload Button 클릭 시 카메라 촬영과 사진 선택 중 선택
    - 촬영 or 선택한 사진을 미리보기 할 수 있도록 구현

--- Study/Hanna/src/screens/home/Home.tsx
+++ Study/Hanna/src/screens/home/Home.tsx
@@ -1,16 +1,84 @@
-import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
-import RequestPermission from '../../components/permission/requestPermission';
+import React, {useEffect, useState} from 'react';
+import {Button, Image, Platform, View} from 'react-native';
+import RequestPermission from '../../components/permission/RequestPermission';
+import {
+  ImagePickerResponse,
+  MediaType,
+  launchCamera,
+  launchImageLibrary,
+} from 'react-native-image-picker';
+import UploadImageModal from '../../components/Modal/UploadImageModal';
 
 const Home = () => {
+  const [imageSource, setImageSource] = useState<string | null>(null);
+  const [openModal, setOpenModal] = useState(false);
+
+  const options = {
+    mediaType: 'photo' as MediaType,
+    maxWidth: 800,
+    maxHeight: 800,
+    includeBase64: Platform.OS === 'android',
+  };
+
+  const handleOpenCamera = () => {
+    launchCamera(options, (response: ImagePickerResponse) => {
+      if (response.didCancel) {
+        console.log('User cancelled camera capture');
+      } else if (response.errorMessage) {
+        console.log('ImagePicker Error: ', response.errorMessage);
+      } else if (response.assets && response.assets.length > 0) {
+        const selectedImage = response.assets[0];
+        if (selectedImage.uri) {
+          // selectedImage.uri가 undefined인 경우에도 setImageSource를 호출하면 안됨
+          setImageSource(selectedImage.uri);
+        }
+      }
+    });
+  };
+
+  const handleOpenGallery = () => {
+    launchImageLibrary(options, response => {
+      if (!response.didCancel && response.assets) {
+        const selectedImage = response.assets[0];
+        if (selectedImage.uri) {
+          setImageSource(selectedImage.uri);
+        }
+      }
+    });
+  };
+
+  const handleOpenModal = () => {
+    if (Platform.OS === 'android') {
+      setOpenModal(true);
+      setImageSource(null);
+    }
+  };
+
   useEffect(() => {
     RequestPermission();
   }, []);
 
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <>
+      <View>
+        {imageSource ? (
+          <Image
+            source={{uri: imageSource}}
+            style={{width: 300, height: 300}}
+          />
+        ) : (
+          <>
+            <UploadImageModal
+              visible={openModal}
+              onClose={() => setOpenModal(false)}
+              onCamera={handleOpenCamera}
+              onGallery={handleOpenGallery}
+            />
+          </>
+        )}
+        <Button title="Upload" onPress={handleOpenModal} />
+      </View>
+    </>
   );
 };
 
