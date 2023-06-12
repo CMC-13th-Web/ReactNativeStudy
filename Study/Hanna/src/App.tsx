dfe63dda2353ed71f19771e478c5573cb3a23f59
Hanna922 <hanna.urssu@gmail.com>
2023-06-12 17:00:23 +0900

    feat #1 add SplashScreen
    - 2초동안 활성화 되도록 구현
    - splash screen 세부 구현 코드는 android 폴더 내부에 위치

--- Study/Hanna/src/App.tsx
+++ Study/Hanna/src/App.tsx
@@ -5,10 +5,21 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import Home from './screens/home/Home';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    try {
+      setTimeout(() => {
+        SplashScreen.hide();
+      }, 2000);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
   return <Home />;
 };
 
