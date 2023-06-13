a08dbfc1c9c82ac5501d5c553fb5182752fc54cf
hamo-o <yheel369@gmail.com>
2023-06-13 11:43:04 +0900

    feat #1 이미지 보여주기 및 컴포넌트화

--- Study/Ham/Week2/src/App.tsx
+++ Study/Ham/Week2/src/App.tsx
@@ -1,9 +1,6 @@
 import React, {useEffect} from 'react';
-import {Button, View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import {useCameraPermissions} from './hooks/useCameraPermissions';
-import useOpenCamera from './hooks/useOpenCamera';
-import useOpenGallery from './hooks/useOpenGallery';
+import Home from './pages/home';
 
 const App = () => {
   useEffect(() => {
@@ -16,19 +13,7 @@ const App = () => {
     }
   }, []);
 
-  const check = useCameraPermissions({
-    title: '카메라 권한 요청',
-    message: '카메라 접근을 허용해주세요.',
-    buttonPositive: '확인',
-  });
-  check.requestPermission();
-
-  return (
-    <View>
-      <Button title="카메라로 설정하기" onPress={useOpenCamera} />
-      <Button title="사진 선택하기" onPress={useOpenGallery} />
-    </View>
-  );
+  return <Home />;
 };
 
 export default App;
---
+++ Study/Ham/Week2/src/pages/home/index.tsx
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import {Button, Image, View} from 'react-native';
+import {useCameraPermissions} from '../../hooks/useCameraPermissions';
+import ImagePicker from 'react-native-image-crop-picker';
+
+const Home = () => {
+  const check = useCameraPermissions({
+    title: '카메라 권한 요청',
+    message: '카메라 접근을 허용해주세요.',
+    buttonPositive: '확인',
+  });
+  check.requestPermission();
+
+  const [img, setImg] = useState<string | null>(null);
+
+  const handlePressOpenCamera = async () => {
+    try {
+      const image = await ImagePicker.openCamera({
+        width: 300,
+        height: 400,
+        cropping: true,
+      });
+      setImg(image?.path);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  const handlePressOpenGallery = async () => {
+    try {
+      const image = await ImagePicker.openPicker({
+        width: 300,
+        height: 400,
+        cropping: true,
+      });
+      setImg(image?.path);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  return (
+    <View>
+      {img && <Image source={{uri: img}} style={{width: 300, height: 300}} />}
+      <Button title="카메라로 설정하기" onPress={handlePressOpenCamera} />
+      <Button title="사진 선택하기" onPress={handlePressOpenGallery} />
+    </View>
+  );
+};
+
+export default Home;
