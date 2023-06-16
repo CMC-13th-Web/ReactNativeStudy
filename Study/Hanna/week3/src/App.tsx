56c0dccd0f12732dd3b98c075b2cdb1824798c4f
Hanna922 <hanna.urssu@gmail.com>
2023-06-17 03:22:31 +0900

    chore: initial commit

---
+++ Study/Hanna/week3/src/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>week3</Text>
+    </>
+  );
+};
+
+export default App;
