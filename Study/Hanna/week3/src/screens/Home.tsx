5f5236a12c2b73e5b102a4c67df7db4b76a54b7d
Hanna922 <hanna.urssu@gmail.com>
2023-06-18 17:40:26 +0900

    feat #1 현재 위치 가져오기 구현

--- Study/Hanna/week3/src/screens/Home.tsx
+++ Study/Hanna/week3/src/screens/Home.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import Geolocation from '@react-native-community/geolocation';
+import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
-export const Home = () => {
+export const Home = ({}) => {
+  const [latitude, setLatitude] = useState(37);
+  const [longitude, setLongitude] = useState(37);
+
+  useEffect(() => {
+    geoLocation();
+  }, [latitude, longitude]);
+
+  const geoLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const currentLat = JSON.stringify(position.coords.latitude);
+        const currentLon = JSON.stringify(position.coords.longitude);
+        setLatitude(Number(currentLat));
+        setLongitude(Number(currentLon));
+      },
+      error => console.log(error.code, error.message),
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  };
+
   return (
     <>
       <View style={{flex: 1}}>
@@ -10,11 +31,12 @@ export const Home = () => {
           style={{flex: 1}}
           provider={PROVIDER_GOOGLE}
           initialRegion={{
-            latitude: 37.5326,
-            longitude: 127.024612,
+            latitude: latitude,
+            longitude: longitude,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
+          showsUserLocation={true}
         />
       </View>
     </>
