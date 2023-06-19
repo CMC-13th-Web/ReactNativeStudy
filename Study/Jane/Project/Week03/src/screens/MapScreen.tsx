773370db9d6fc1124ab513fa3fd7153956abe81a
Yeonju Jo <yjj0287@gmail.com>
2023-06-20 07:59:32 +0900

    feat: naver map과 현재 위치 marker 추가

--- Study/Jane/Project/Week03/src/screens/MapScreen.tsx
+++ Study/Jane/Project/Week03/src/screens/MapScreen.tsx
@@ -1,23 +1,47 @@
-import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
+import NaverMapView, {Coord, Marker} from 'react-native-nmap';
 
 export default function MapScreen() {
+  const [currentPosition, setCurrentPosition] = useState<Coord>({
+    latitude: 37.565383,
+    longitude: 126.976292,
+  });
+
   useEffect(() => {
     Geolocation.getCurrentPosition(
       position => {
-        console.log(position);
+        setCurrentPosition({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
       },
       error => {
         console.log(error.code, error.message);
       },
-      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      {
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 10000,
+        distanceFilter: 10,
+      },
     );
   }, []);
 
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={styles.full}>
+      <NaverMapView
+        style={{width: '100%', height: '100%'}}
+        center={{...currentPosition, zoom: 16}}>
+        <Marker coordinate={currentPosition} pinColor="blue" />
+      </NaverMapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
