708319652dddf8af8a19645bc0ddf4fc9ade9225
Yeonju Jo <yjj0287@gmail.com>
2023-06-20 02:02:21 +0900

    feat: 현재 위치 가져오기 기능 추가

--- Study/Jane/Project/Week03/src/screens/MapScreen.tsx
+++ Study/Jane/Project/Week03/src/screens/MapScreen.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 
 export default function MapScreen() {
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  }, []);
+
   return (
     <View>
       <Text>Map</Text>
