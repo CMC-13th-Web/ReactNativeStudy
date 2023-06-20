386c175da01d639835990b0572d91a70ff7470ba
kongnayeon <seoultownroad@naver.com>
2023-06-20 20:19:49 +0900

    feat: 현재 위치, 마커 색 커스텀

--- Study/JJang/src/screens/search/index.tsx
+++ Study/JJang/src/screens/search/index.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
-
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable @typescript-eslint/no-shadow */
+import React, {useState} from 'react';
 import NaverMapView, {Marker} from 'react-native-nmap';
+import Geolocation from '@react-native-community/geolocation';
 
 export const SearchScreen = () => {
-  const P0 = {latitude: 37.564362, longitude: 126.977011};
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLogitude] = useState(0);
+
+  const geoLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const latitude = Number(JSON.stringify(position.coords.latitude));
+        const longitude = Number(JSON.stringify(position.coords.longitude));
+
+        setLatitude(latitude);
+        setLogitude(longitude);
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  };
+
+  geoLocation();
 
   return (
     <NaverMapView
       style={{width: '100%', height: '100%'}}
       showsMyLocationButton={true}
-      center={{...P0, zoom: 16}}
+      center={{latitude: latitude, longitude: longitude, zoom: 16}}
       onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
       onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}>
-      <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')} />
+      <Marker
+        coordinate={{latitude: latitude, longitude: longitude}}
+        pinColor="#9abff3"
+      />
     </NaverMapView>
   );
 };
